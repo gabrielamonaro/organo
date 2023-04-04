13fd7c491762e5becf106a13d910675433614505
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-04-04 19:43:00 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './Components/Banner';
+import CampoTexto from './Components/CampoTexto';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner></Banner>
+      <CampoTexto label="Nome"  placeholder="Digite seu nome"></CampoTexto>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"></CampoTexto>
     </div>
+    
   );
 }
 
---
+++ src/Components/Banner/index.js
@@ -0,0 +1,12 @@
+ import './style.css'
+
+function Banner ()
+{
+    return (
+    <header>
+        <h1> <img src="/imagens/banner.png" alt="Banner principal da página do Organo."/> </h1>
+    </header>
+    )
+}
+
+export default Banner
